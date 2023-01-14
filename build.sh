905a005e7fa3fcfea9f07cd8422f9995961bcce8
Alessio Morale <alessiomorale@gmail.com>
2023-01-14 13:15:09 +0000

    fix: update vscode settings, build script

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 mkdir -p build && cd build
-cmake .. -DCMAKE_VERBOSE_MAKEFILE=true -DCMAKE_BUILD_TYPE=Debug -DCMAKE_EXPORT_COMPILE_COMMANDS=1
+cmake .. -DCMAKE_BUILD_TYPE=Debug -DCMAKE_EXPORT_COMPILE_COMMANDS=1
 cmake --build . -j $@
